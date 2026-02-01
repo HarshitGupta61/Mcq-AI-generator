79544a7e6cc6e1afe13557aa2106aa2eb82bd6a2
Harshit Gupta <harshitgupta6121L@gmail.com>
2026-02-01 09:14:44 +0530

    Remove dotenv and fix Groq setup for Streamlit Cloud

--- llm.py
+++ llm.py
@@ -1,10 +1,13 @@
 import os
 import json
 from groq import Groq
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
-client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+# load_dotenv()
+
+client = Groq(api_key=os.environ["GROQ_API_KEY"])
+
+# client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 MAX_CHARS = 4000  # 🔥 Prevent token overflow
 
